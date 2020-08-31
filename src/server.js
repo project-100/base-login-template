715740e601fbc5e8ada31fe057f2e2feb0e8e3eb
Mugilan-Codes <mugilancodes@gmail.com>
2020-08-31 11:45:30 +0530

    Unrecognized Routes

--- src/server.js
+++ src/server.js
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
   res.send('API Running...');
 });
 
+app.use((req, res) => {
+  res.status(404).send('404 Not Found');
+});
+
 app.listen(api_port, () => {
   console.log(
     `Sever started on port ${api_port} -> http://localhost:${api_port}`
